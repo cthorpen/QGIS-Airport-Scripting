59063b95f5f8f58767a388aadb099f13f11d8961
Cole Thorpen <thorpencole@gmail.com>
2023-06-07 23:08:17 -0700

    cant get module installed

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from qgis.core import *
 
 # supply path to qgis install location
-QgsApplication.sexPrefixPath("/Users/ColeThorpen/Desktop", True)
+QgsApplication.sexPrefixPath('/Applications/QGIS.app/Contents/MacOS', True)
 
 # create a reference to the QgsApplication
 # setting the second argument to False disables the GUI
 qgs = QgsApplication([], False)
 
-#load providers
+# load providers
 qgs.initQgis()
 
 # write coder here to load layers, processing, algs, etc.
